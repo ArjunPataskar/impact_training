14ab8da3db72d3c584e65b1a2d657810b5fac2b9
Arjun Pataskar <arjunspataskar@gmail.com>
2025-05-19 08:17:11 +0530

    a

---
+++ checksetbit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class checksetbit {
+    public static void main(String[] args) {
+        System.out.println("Enter the binary value : ");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("Enter the index value for checking : ");
+        int b=sc.nextInt();
+        for(int i=0i<b;i++){
+            a=
+        }
+    }
+}
